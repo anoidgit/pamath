57e2a8adecd37648d7c652c698612a97c077ce26
ano <anoidgit@users.noreply.github.com>
2017-01-19 10:25:12 +0800

    Update compile.sh

--- linux/compile.sh
+++ linux/compile.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-infile=pamath_core.cpp
-ofile=pamath_core.so
+
+infile=double_pamath_core.cpp
+ofile=double_pamath_core.so
+g++ -o $ofile -Ofast -fopenmp -shared -fPIC $infile
+
+infile=float_pamath_core.cpp
+ofile=float_pamath_core.so
+g++ -o $ofile -Ofast -fopenmp -shared -fPIC $infile
+
+infile=long_double_pamath_core.cpp
+ofile=long_double_pamath_core.so
 g++ -o $ofile -Ofast -fopenmp -shared -fPIC $infile
