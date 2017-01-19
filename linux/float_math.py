1ddcdc2dfc2241977a3c6058a61350ff3def8a03
ano <anoidgit@users.noreply.github.com>
2017-01-19 11:23:39 +0800

    Create float_math.py

---
+++ linux/float_math.py
@@ -0,0 +1,28 @@
+from ctypes import cdll, byref, c_long, c_float
+
+lib_pamath = cdll.LoadLibrary("./float_math_core.so")
+lib_pamath.sum_vec.restype = c_float
+lib_pamath.dot_vec.restype = c_float
+lib_pamath.cos_vec.restype = c_float
+
+def sum_vec(vl):
+	ilen = len(vl)
+	Vec = c_float * ilen
+	return lib_pamath.sum_vec(byref(Vec(*vl)), c_long(ilen))
+
+def dot_vec(v1, v2):
+	ilen = len(v1)
+	Vec = c_float * ilen
+	return lib_pamath.dot_vec(byref(Vec(*v1)), byref(Vec(*v2)), c_long(ilen))
+
+def cos_vec(v1, v2):
+	ilen = len(v1)
+	Vec = c_float * ilen
+	return lib_pamath.cos_vec(byref(Vec(*v1)), byref(Vec(*v2)), c_long(ilen))
+
+def mul_vec(v1, v2):
+	ilen = len(v1)
+	Vec = c_float * ilen
+	vrs = Vec()
+	lib_pamath.mul_vec(byref(Vec(*v1)), byref(Vec(*v2)), byref(vrs), c_long(ilen))
+	return list(vrs)
