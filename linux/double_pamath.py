b77b4ab74d1bb8b74ef3635b037f7770728b9e43
ano <anoidgit@users.noreply.github.com>
2017-01-19 11:17:34 +0800

    Update double_pamath.py

--- linux/double_pamath.py
+++ linux/double_pamath.py
@@ -1,51 +1,28 @@
-from ctypes import cdll, pointer, c_long, c_double
+from ctypes import cdll, byref, c_long, c_double
 
 lib_pamath = cdll.LoadLibrary("./double_pamath_core.so")
 lib_pamath.sum_vec.restype = c_double
 lib_pamath.dot_vec.restype = c_double
 lib_pamath.cos_vec.restype = c_double
 
-def copyl(lin, ai):
-	curid = 0
-	for lu in lin:
-		ai[curid] = lu
-		curid += 1
-
-def genl(vi, vlen):
-	return [vi[i] for i in xrange(vlen)]
-
 def sum_vec(vl):
-	vlen = c_long(len(vl))
 	ilen = len(vl)
-	vi = (c_double * ilen)()
-	copyl(vl, vi)
-	return lib_pamath.sum_vec(pointer(vi), vlen)
+	Vec = c_double * ilen
+	return lib_pamath.sum_vec(byref(Vec(*vl)), c_long(ilen))
 
 def dot_vec(v1, v2):
-	vlen = c_long(len(v1))
 	ilen = len(v1)
-	vi1 = (c_double * ilen)()
-	copyl(v1, vi1)
-	vi2 = (c_double * ilen)()
-	copyl(v2, vi2)
-	return lib_pamath.dot_vec(pointer(vi1), pointer(vi2), vlen)
+	Vec = c_double * ilen
+	return lib_pamath.dot_vec(byref(Vec(*v1)), byref(Vec(*v2)), c_long(ilen))
 
 def cos_vec(v1, v2):
-	vlen = c_long(len(v1))
 	ilen = len(v1)
-	vi1 = (c_double * ilen)()
-	copyl(v1, vi1)
-	vi2 = (c_double * ilen)()
-	copyl(v2, vi2)
-	return lib_pamath.cos_vec(pointer(vi1), pointer(vi2), vlen)
+	Vec = c_double * ilen
+	return lib_pamath.cos_vec(byref(Vec(*v1)), byref(Vec(*v2)), c_long(ilen))
 
 def mul_vec(v1, v2):
-	vlen = c_long(len(v1))
 	ilen = len(v1)
-	vi1 = (c_double * ilen)()
-	copyl(v1, vi1)
-	vi2 = (c_double * ilen)()
-	copyl(v2, vi2)
-	vrs = (c_double * ilen)()
-	lib_pamath.mul_vec(pointer(vi1), pointer(vi2), pointer(vrs), vlen)
-	return genl(vrs, ilen)
+	Vec = c_double * ilen
+	vrs = Vec()
+	lib_pamath.mul_vec(byref(Vec(*v1)), byref(Vec(*v2)), byref(vrs), c_long(ilen))
+	return list(vrs)
