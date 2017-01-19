868cdff2548d3f466ed0eeecf5ee1d89982d3859
ano <anoidgit@users.noreply.github.com>
2017-01-19 11:14:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 #from float_pamath import *
 #from double_pamath import *
 #from long_double_pamath import *
-from py_pamath import *
+
+#from float_math import *
+#from double_math import *
+#from long_double_math import *
+
+from py_math import *
+
 from tqdm import tqdm
 
-li=[float(i+1)/1024 for i in xrange(1024)]
-for i in tqdm(xrange(1000)):
+li=[float(i+1)/4096 for i in xrange(8192)]
+for i in tqdm(xrange(100)):
 	sum_vec(li)
 	dot_vec(li,li)
 	cos_vec(li,li)
